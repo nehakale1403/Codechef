1a13f4845b1d87380a5863e5410709c9eb8dd087
Neha Kale <nkale8013@gmail.com>
2021-01-25 10:35:14 +0530

    sum of all digits of a number

---
+++ Practice_problems/FLOW006.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    int n, sum=0;
+	    cin>>n;
+	    while(n != 0){
+	        sum = sum + n%10;
+	        n = n/10;
+	    }
+	    cout<<sum<<endl;
+	}
+	return 0;
+}
