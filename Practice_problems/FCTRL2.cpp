9ce8de7242864febf11da81a89c9f974d287409b
Neha Kale <nkale8013@gmail.com>
2021-01-29 10:39:45 +0530

    wrote the program in java using BigInteger

--- Practice_problems/FCTRL2.cpp
+++ Practice_problems/FCTRL2.cpp
@@ -1,19 +1,43 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int main(){
+// int main(){
     
-    int t;
-    cin>>t;
+//     int t;
+//     cin>>t;
     
-    while(t--){
-        int n, fact;
-        cin>>n;
-        fact=n;
-        while(--n){
-            fact = fact*n;
-        }
-        cout<<fact<<endl;
+//     while(t--){
+//         int n, fact;
+//         cin>>n;
+//         fact=n;
+//         while(--n){
+//             fact = fact*n;
+//         }
+//         cout<<fact<<endl;
         
-    }
+//     }
+// }
+
+import java.util.Scanner;
+import java.math.BigInteger;
+public class Main{
+ public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     int n,t,cs=1;
+     t = sc.nextInt();
+     
+     while(t>=cs){
+         n = sc.nextInt();
+
+     BigInteger factorial = BigInteger.ONE;
+
+     for (int i=1; i<=n; i++) {
+         factorial = factorial.multiply(BigInteger.valueOf(i));
+     }
+     System.out.println(factorial);
+     cs=cs+1;
+     }
+
+
+ }
 }
