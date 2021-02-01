2ca314ba9e498b48bbf99048d059d79abe9cb257
Neha Kale <nkale8013@gmail.com>
2021-02-01 23:47:45 +0530

    Added questions

--- Jan21_LunchTime/EvenDifferences.cpp
+++ Jan21_LunchTime/EvenDifferences.cpp
@@ -1,3 +1,9 @@
+// You have an array consisting of N integers a1,a2...aN. You may perform the following operation zero or more times.
+
+// Choose any index i (1≤i≤N) and set ai:=ai+1
+// Determine the minimum number of operations you have to perform so that all pairwise differences in the resulting array are even.
+// More formally, in the resulting array it should hold that, for every i and j (1≤i,j≤N), ai−aj is even.
+
 #include <iostream>
 using namespace std;
 
