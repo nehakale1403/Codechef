8cb236c0dae037af02ff4a3054e471a77e0e7c62
Neha Kale <nkale8013@gmail.com>
2021-02-20 17:24:04 +0530

    added first and last digit of an integer

---
+++ Practice_problems/FLOW004.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n,first, last;
+        cin>>n;
+        last = n%10;
+        while(n>10){
+            n=n/10;
+        }
+        first=n;
+        
+        cout<<(first+last)<<endl;
+        
+    }
+	return 0;
+}
