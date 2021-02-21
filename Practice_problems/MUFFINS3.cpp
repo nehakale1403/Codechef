a75ba9678c9c203ef3725b59b094fd0146320d94
Neha Kale <nkale8013@gmail.com>
2021-02-21 22:20:46 +0530

    chef and muffins

---
+++ Practice_problems/MUFFINS3.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int size,n;
+        cin>>n;
+        size=(n/2)+1;
+        cout<<size<<endl;
+    }
+	return 0;
+}
