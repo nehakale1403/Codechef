b03a58a76e933cef398fb527048d91419cabe614
Neha Kale <nkale8013@gmail.com>
2021-02-21 21:41:57 +0530

    Second largest number

---
+++ Practice_problems/FLOW017.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    int arr[3];
+	    arr[0]=a;
+	    arr[1]=b;
+	    arr[2]=c;
+	    sort(arr, arr+3);
+	    cout<<arr[1]<<endl;
+	}
+	return 0;
+}
