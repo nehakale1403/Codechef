defc8ecec3ee80ae3b5e71f6c116163c3366ffea
Neha Kale <nkale8013@gmail.com>
2021-02-24 12:46:03 +0530

    Id and Ship

---
+++ Practice_problems/FLOW010.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    char i;
+	    cin>>i;
+	    
+	    if(i == 'b' || i == 'B')
+	        cout<<"BattleShip"<<endl;
+	    else if(i == 'c' || i == 'C')
+	        cout<<"Cruiser"<<endl;
+	    else if(i == 'd' || i == 'D')
+	        cout<<"Destroyer"<<endl;
+	    else if(i == 'f' || i == 'F')
+	        cout<<"Frigate"<<endl;
+	}
+	return 0;
+}
